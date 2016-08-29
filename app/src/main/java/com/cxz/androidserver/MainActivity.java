7c284a7bf7ca191a37989331ea7c0b4f560ba1e8
chenxz <2163623522@qq.com>
2016-08-29 21:43:27 +0800

    实现WiFi传图

--- app/src/main/java/com/cxz/androidserver/MainActivity.java
+++ app/src/main/java/com/cxz/androidserver/MainActivity.java
@@ -1,8 +1,12 @@
 package com.cxz.androidserver;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.IOException;
 
@@ -20,8 +24,26 @@ public class MainActivity extends AppCompatActivity {
         webConfig.setMaxParallels(50);
         shs = new SimpleHttpServer(webConfig);
         shs.registerResourceHandler(new ResourceInAssetsHandler(this));
-        shs.registerResourceHandler(new UploadImageHandler());
+        shs.registerResourceHandler(new UploadImageHandler(){
+            @Override
+            protected void onImageLoaded(String path){
+                showImage(path);
+            }
+        });
         shs.startAsync();
+    }
+
+    private void showImage(final String path) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                ImageView iv = (ImageView) findViewById(R.id.imageView);
+                Bitmap bitmap = BitmapFactory.decodeFile(path);
+                iv.setImageBitmap(bitmap);
+                Toast.makeText(MainActivity.this,"image load and show",Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/cxz/androidserver/UploadImageHandler.java
+++ app/src/main/java/com/cxz/androidserver/UploadImageHandler.java
@@ -1,6 +1,8 @@
 package com.cxz.androidserver;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 
@@ -18,12 +20,33 @@ public class UploadImageHandler implements IResourceUriHandler {
 
     @Override
     public void handle(String uri, HttpContext httpContext) throws IOException {
+//        OutputStream os = httpContext.getUnderlySocket().getOutputStream();
+//        PrintWriter writer = new PrintWriter(os);
+//        writer.println("HTTP/1.1 200 OK");
+//        writer.println();
+//        writer.println("from upload image handler");
+//        writer.flush();
+
+        String tmpPath = "/mnt/sdcard/test_upload.jpg";
+        long totalLength = Long.parseLong(httpContext.getRequestHeaderValue("Content-Length"));
+        FileOutputStream fos = new FileOutputStream(tmpPath);
+        InputStream is = httpContext.getUnderlySocket().getInputStream();
+        byte[] buffer = new byte[1024];
+        int nReaded = 0;
+        long nLeftLength = totalLength;
+        while ((nReaded = is.read(buffer)) > 0 && nLeftLength > 0){
+            fos.write(buffer,0,nReaded);
+            nLeftLength -= nReaded;
+        }
+        fos.close();
+
         OutputStream os = httpContext.getUnderlySocket().getOutputStream();
-        PrintWriter writer = new PrintWriter(os);
-        writer.println("HTTP/1.1 200 OK");
-        writer.println();
-        writer.println("from upload image handler");
-        writer.flush();
+        PrintWriter printer = new PrintWriter(os);
+        printer.println("HTTP/1.1 200 OK");
+        printer.println();
+
+        onImageLoaded(tmpPath);
     }
 
+    protected  void onImageLoaded(String path){}
 }
